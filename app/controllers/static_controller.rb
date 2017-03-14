d6ac417092e974d89f73420fbb52714c14ea22e7
tgt87 <golduck21187@yahoo.com.sg>
2017-03-14 14:45:34 +0800

    recent prop added

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -3,6 +3,6 @@ class StaticController < ApplicationController
     #From property database, pick out top 3 popular properties
 
     #From Property database, pick out 3 most recent properties
-
+    @recent_properties = Property.order('created_at DESC').limit(3)
   end
 end
