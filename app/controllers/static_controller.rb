2d4cc69d1c7240c02eeda3dbb3356f5da1e58326
tgt87 <golduck21187@yahoo.com.sg>
2017-03-14 17:34:38 +0800

    skeleton homepage done

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,6 +2,11 @@ class StaticController < ApplicationController
   def homepage
     #From property database, pick out top 3 popular properties
 
+    @properties = Property.all
+    @sorted_properties = @properties.sort_by {|prop| prop.shortlists.count}
+    @sorted_properties.reverse!
+    @top_shortlists = [@sorted_properties[0], @sorted_properties[1], @sorted_properties[2]]
+
     #From Property database, pick out 3 most recent properties
     @recent_properties = Property.order('created_at DESC').limit(3)
   end
