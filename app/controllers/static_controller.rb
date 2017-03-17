94855426d1cfbfe2e2e9f435aff34c5ab10953ea
tgt87 <golduck21187@yahoo.com.sg>
2017-03-17 13:40:42 +0800

    fix homepage bug

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,14 +1,27 @@
 class StaticController < ApplicationController
   def homepage
-    #From property database, pick out top 3 popular properties
 
     @properties = Property.all
-    @sorted_properties = @properties.sort_by {|prop| prop.shortlists.count}
-    @sorted_properties.reverse!
-    @top_shortlists = [@sorted_properties[0], @sorted_properties[1], @sorted_properties[2]]
+    @prop_length = @properties.length
+    i = 0
+    @top_shortlists = []
+    if !@properties.empty?
+      #From property database, pick out top 3 popular properties
+      @sorted_properties = @properties.sort_by {|prop| prop.shortlists.count}
+      @sorted_properties.reverse!
 
-    #From Property database, pick out 3 most recent properties
-    @recent_properties = Property.order('created_at DESC').limit(3)
+      if @prop_length > 3
+          @prop_length = 3
+      end
+
+      while i < @prop_length
+          @top_shortlists.push(@sorted_properties[i])
+          i += 1
+      end
+
+      #From Property database, pick out 3 most recent properties
+      @recent_properties = Property.order('created_at DESC').limit(3)
+    end
 
     @all_properties = []
     @properties.each { |p| @all_properties.push(p.address) }
